b2099c9e2074352dc31b53e8f9b015fb983b6125
Clint Bowersock <52680761+cbowersock@users.noreply.github.com>
2020-04-22 18:33:52 -0400

    module 5

--- app.js
+++ app.js
@@ -14,8 +14,14 @@ app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dis
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render('index', {
+    title: 'library',
+    list: ['books', 'other stuff'],
+  });
 });
 
 app.listen(port, () => {
